6b2f693e383175004f34bbdf7db8a97ab7ee6103
calvin <74103103+Calvindd2f@users.noreply.github.com>
2022-09-29 23:06:12 +0100

    Update Check_Chrome_version.ps1

--- Security/Check_Chrome_version.ps1
+++ Security/Check_Chrome_version.ps1
@@ -1,10 +1,7 @@
 ## Getting Chrome version; because in the current climate the zero in zero-day stands for the amount of days since the last 0-Day
 
-$ResultWow6432 = (Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object { $_ -match "Chrome" } | Select-Object -ExpandProperty DisplayVersion)
-$Result = (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object { $_ -match "Chrome" } | Select-Object -ExpandProperty DisplayVersion)
+$ResultWow6432 = (Get-ItemProperty -Path HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object { $_ -match 'Chrome' } | Select-Object -ExpandProperty DisplayVersion)
+$Result = (Get-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object { $_ -match 'Chrome' } | Select-Object -ExpandProperty DisplayVersion)
 
-if ($ResultWow6432) {
-    
-}
-Write-Output "Version Wow6432: $($ResultWow6432)`r"
-Write-Output "Version: $($Result)`r"
+Write-Output -InputObject ("Version Wow6432: {0}`r" -f ($ResultWow6432))
+Write-Output -InputObject ("Version: {0}`r" -f ($Result))
