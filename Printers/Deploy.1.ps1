60003d8567f2fb5e5c5884c2dfaa36fca20ef3f2
Calvin <74103103+Calvindd2f@users.noreply.github.com>
2022-12-01 20:45:10 +0000

    Update Deploy.1.ps1

--- Printers/Deploy.1.ps1
+++ Printers/Deploy.1.ps1
@@ -1,24 +1,31 @@
+$PrintNick = '211 Call Center'
+$IP_OEM = '1234 Xerox'
+$printerIP = '1.2.3.4'
+$DriverDir = "$env:windir\System32\DriverStore\FileRepository\xeroxaltalinkc81xx_pcl6.inf_amd64_cd97e831cee685b7\XeroxAltaLinkC81xx_PCL6.inf"
+$PrinterName = 'Xerox AltaLink C8145 V4 PCL6'
+$inf = '.\drivers\XeroxAltaLinkC81xx_PCL6.inf'
+$driverzip = '.\drivers.zip'
 set-ErrorAction SilentlyContinue
-Expand-Archive -Path .\drivers.zip -DestinationPath .\
+Expand-Archive -Path $driverzip -DestinationPath .\
 
 if ($env:USERDNSDomai -eq $null)
 {
-pnputil.exe /a ".\drivers\XeroxAltaLinkC81xx_PCL6.inf"
-Add-PrinterDriver -Name "Xerox AltaLink C8145 V4 PCL6" -InfPath "C:\Windows\System32\DriverStore\FileRepository\xeroxaltalinkc81xx_pcl6.inf_amd64_cd97e831cee685b7\XeroxAltaLinkC81xx_PCL6.inf"
-Add-PrinterPort -Name "1234 Xerox" -PrinterHostAddress "1.2.3.4"
-Add-Printer -DriverName "Xerox AltaLink C8145 V4 PCL6" -Name "1234 Xerox" -PortName "1234 Xerox"
+& "$env:windir\system32\pnputil.exe" /a $inf
+Add-PrinterDriver -Name $PrinterName -InfPath $DriverDir
+Add-PrinterPort -Name $IP_OEM -PrinterHostAddress $printerIP
+Add-Printer -DriverName $PrinterName -Name $IP_OEM -PortName $IP_OEM
 }
 
-Write-Host "Diag"
-Write-Host "Site Name:" $env:CS_PROFILE_NAME
-Write-Host ""
-Write-Host ""
-Get-Item -Path ".\drivers.zip"
-Get-Item -Path "C:\Windows\System32\DriverStore\FileRepository\xeroxaltalinkc81xx_pcl6.inf_amd64_cd97e831cee685b7\XeroxAltaLinkC81xx_PCL6.inf"
-Get-Item -Path ".\drivers\XeroxAltaLinkC81xx_PCL6.inf"
-Write-Host ""
-ping 1.2.3.4
-Write-Host "checking if the printer installed sucessfully"
-Get-Printer -Name "211 Call Center" | Format-List
+Write-Verbose -Message 'Diag'
+Write-Verbose -Message 'Site Name:'
+Write-Verbose -Message ''
+Write-Verbose -Message ''
+Get-Item -Path $driverzip
+Get-Item -Path $DriverDir
+Get-Item -Path $inf
+Write-Verbose -Message ''
+& "$env:windir\system32\ping.exe" $printerIP
+Write-Verbose -Message 'checking if the printer installed sucessfully'
+Get-Printer -Name $PrintNick | Format-List
 
-if ((Get-Printer -Name "211 Call Center" | Format-List) -eq $null) {exit 1}
+if ((Get-Printer -Name $PrintNick | Format-List) -eq $null) {exit 1}
