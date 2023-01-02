b80a859027658a474383061384c7e8122146deef
Calvin <74103103+Calvindd2f@users.noreply.github.com>
2023-01-02 17:46:33 +0000

    Update Microsoft365.ps1

--- Leaver/Microsoft365.ps1
+++ Leaver/Microsoft365.ps1
@@ -13,15 +13,46 @@ $user = user1@example.com
 $method = Get-MgUserAuthenticationPasswordMethod -UserId $user
 Reset-MgUserAuthenticationMethodPassword -UserId $user -RequireChangeOnNextSignIn -AuthenticationMethodId $method.id -NewPassword "zQ7!Ra3MM6ha"
 
+# Reset c@lvin.ie's password
+$passwordProfile = New-Object -TypeName Microsoft.Graph.PasswordProfile
+$passwordProfile.Password = 'GeneratedPassword'
+Set-MSGraphUserPassword -User $user -PasswordProfile $passwordProfile
+
 # Block sign-in
 $params = @{
      AccountEnabled = "false"
              }
  Update-MgUser -UserId $User.Id -BodyParameter $params
  
+ Set-MSGraphUser -User $user -BlockSignIn $true
+ 
+ 
 # Remove from groups
+Get-MSGraphGroup -Tenant lvin.ie | Where-Object {$_.Members -contains $user.id} | ForEach-Object {
+    Remove-MSGraphGroupMember -Group $_ -Member $user
+}
+
 # Change AAD Description
+$userProfile = Get-MSGraphUserProfile -User $user
+$userProfile.AboutMe = "Disabled on $(Get-Date -Format 'dd/MM/yyyy')"
+Set-MSGraphUserProfile -UserProfile $userProfile
+
 # Remove MFA methods, revoke sessions [both MFA and standard]
+Get-MSGraphMFA -Tenant lvin.ie -UserPrincipalName c@lvin.ie | ForEach-Object {
+    Remove-MSGraphMFA -MFA $_
+}
+
 # Disable user
+Set-MSGraphUser -User $user -AccountEnabled $false
 # Double check that mailbox is shared
-# Revoke Licenses
+$Shared = Get-Mailbox -Identity $user -Type Shared
+if ($Shared -eq $True) {
+     # Revoke Licenses
+     Get-MsolUser -UserPrincipalName c@lvin.ie | Select-Object -ExpandProperty Licenses | ForEach-Object {
+    Remove-MsolUserLicense -UserPrincipalName c@lvin.ie -RemoveLicenses $_.AccountSkuId
+}
+}Else{
+Write-Verbose "User licenses were not unassigned as the mailbox was unable to be converted to shared."
+Exit
+}
+
