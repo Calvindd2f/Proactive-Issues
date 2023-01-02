c9b519d3ba88bc3f641be1edb204e875a80a7dc3
Calvin <74103103+Calvindd2f@users.noreply.github.com>
2023-01-02 16:24:07 +0000

    Update LetEnc_Powershell_Wild.ps1

--- Certificates/LetEnc_Powershell_Wild.ps1
+++ Certificates/LetEnc_Powershell_Wild.ps1
@@ -1,10 +1,11 @@
+# Requirements
 Write-Verbose @"
 Windows PowerShell 5.1 
 .NET Framework 4.7.2 (link to check) 
 Possibility to add CNAME in DNS
 "@
 
-
+# Check for Admin
 function CheckCisPriv
 {
 <#
@@ -42,16 +43,28 @@ CheckYourPrivilageCisScum
         return
     }}
 
+# Priv check ; ExecPolicy & Install+Import module
 CheckCisPriv
 Set-ExecutionPolicy Bypass -Scope Process
-Install-Module -Name Posh-ACME
+Install-Module -Name 'Posh-ACME'
 Import-Module -Name Posh-ACME
-Write-Verbose @" Displaying Sample
-New-PACertificate *.dread.ie -AcceptTOS -Contact <your-email> -DnsPlugin AcmeDns -PluginArgs @{ACMEServer='auth.acme-dns.io'} -Install
-@"
+
+# Verbose info for installed module
+Write-Verbose @"
+Displaying Sample:
+New-PACertificate $env:Wildcard-or-Domain -AcceptTOS -Contact $env:ContactEmail -DnsPlugin AcmeDns -PluginArgs $env:PluginArgs -Install
+"@
+
+# Variables: Can be used with DRMM
+$env:Wildcard-or-Domain = *.dread.ie
+$env:ContactEmail = calvindd2f@git.hub
+$env:PluginArgs = @{ACMEServer='auth.acme-dns.io'}
+
+# New Certificate
+New-PACertificate $env:Wildcard-or-Domain -AcceptTOS -Contact $env:ContactEmail -DnsPlugin AcmeDns -PluginArgs $env:PluginArgs  -Install
 
 
-
+# Autorenewal Automation
 $Trigger =New-ScheduledTaskTrigger -At 10:00am -Daily
 $Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "C:\Cert\AutoRenewal.ps1"
 Register-ScheduledTask -TaskName "Certificate AutoRenewal" -Trigger $Trigger -User "$env:USERDOMAIN\$env:USERNAME" -Password '<password>' -Action $Action -RunLevel Highest –Force
